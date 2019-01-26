1ecbd9c77b41bc8cd84ef10c546c94051cd219a1
Kevin Jullien <hi@juke.fr>
2019-01-26 03:45:24 +0100

    .

--- pages/index.js
+++ pages/index.js
@@ -2,67 +2,88 @@ import { isMobile } from "react-device-detect";
 import { Component } from "react";
 import projects from "./projects.json";
 
-class Intro extends Component {
-  render() {
-    return (
-      <section>
-        <h1>juke.fr</h1>
-        <h3>Welcome to my portfolio.</h3>
-        {figure({
-          picture: isMobile
-            ? "/static/mobile_scroll_vertical.gif"
-            : "/static/desktop_scroll_vertical.gif",
-          width: 50,
-          caption: `${
-            isMobile ? "Swipe" : "Scroll"
-          } up and down to change project.`
-        })}
-        {figure({
-          picture: isMobile
-            ? "/static/mobile_scroll_horizontal.gif"
-            : "/static/desktop_scroll_horizontal.gif",
-          width: 50,
-          caption: `${
-            isMobile ? "Swipe left and right" : "Left Click and Right Click"
-          } to learn more about a project.`
-        })}
-        {figure({
-          picture: isMobile
-            ? "/static/mobile_tap.gif"
-            : "/static/desktop_click.gif",
-          width: 50,
-          caption: `${isMobile ? "Tap" : "Click"} anywhere to get started.`
-        })}
-        {!isMobile && (
-          <h3>
-            You can also use your keyboard arrow keys if that's your thing.
-          </h3>
-        )}
-        <style jsx>{`
-          section {
-            width: 100vw;
-            height: 100vh;
-            margin: 0;
-            padding: 0;
-            position: fixed;
-            top: 0;
-            left: 0;
-            background: rgba(255, 255, 255, 0.85);
-            z-index: 5;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-          }
-        `}</style>
-      </section>
-    );
-  }
-}
+const intro = ({ isMobile }) => {
+  return (
+    <section>
+      <h1>juke.fr</h1>
+      <h3 className="welcome">Welcome to my portfolio.</h3>
+      <img
+        src={isMobile ? "/static/icons/swipev.svg" : "/static/icons/mousev.svg"}
+        width="auto"
+        height="75px"
+      />
+      <h3>{isMobile ? "Swipe" : "Scroll"} up and down to change project.</h3>
+      <img
+        src={isMobile ? "/static/icons/swipeh.svg" : "/static/icons/mouseh.svg"}
+        width="auto"
+        height="75px"
+      />
+      <h3>
+        {isMobile ? "Swipe left and right" : "Left Click and Right Click"} to
+        learn more about a project.
+      </h3>
+      <img
+        src={isMobile ? "/static/icons/tap.svg" : "/static/icons/click.svg"}
+        width="auto"
+        height="75px"
+      />
+      <h3>{isMobile ? "Tap" : "Click"} anywhere to get started.</h3>
+
+      {!isMobile && (
+        <img src="/static/icons/arrows.svg" width="auto" height="75px" />
+      )}
+      {!isMobile && <h3>You can also use your keyboard arrow keys.</h3>}
+      <style jsx>{`
+        * {
+          margin: 0;
+          padding: 0;
+        }
+
+        h1 {
+          border: 1px solid black;
+          font-size: 21px;
+          text-transform: uppercase;
+          padding: 10px;
+          margin-bottom: 10px;
+        }
+
+        h3 {
+          margin: 0 20px;
+          font-size: 18px;
+          text-align: center;
+          margin-bottom: 30px;
+        }
+
+        h3.welcome {
+          margin-bottom: 40px;  
+        }
+
+        section {
+          width: 100vw;
+          height: 100vh;
+          margin: 0;
+          padding: 0;
+          position: fixed;
+          top: 0;
+          left: 0;
+          background: rgba(255, 255, 255, 0.90);
+          z-index: 5;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+    </section>
+  );
+};
 
 const figure = ({ picture, caption, width = "100%", height = "100%" }) => (
   <figure>
     <img src={picture} />
-    <figcaption>{caption}</figcaption>
+    <figcaption>
+      <h3>{caption}</h3>
+    </figcaption>
     <style jsx>{`
       figure {
         width: ${width};
@@ -75,14 +96,14 @@ const figure = ({ picture, caption, width = "100%", height = "100%" }) => (
 
       img {
         width: 100%;
-        height: 95%;
+        height: 92%;
         margin: 0;
         padding: 0;
       }
 
       figcaption {
         width: 100%;
-        height: 5%;
+        height: 8%;
         margin: 0;
         padding: 0;
       }
@@ -105,7 +126,7 @@ const swiper = ({ projects, swipeAction, transition }) => (
   </section>
 );
 
-const handleProjects = ({ projects, indexY = 0, indexX = 0 }) => {
+const handleProjects = ({ projects, indexY = 0, indexX = 0, isMobile }) => {
   const indexYA =
     indexY - Math.floor(indexY / projects.length) * projects.length;
   const indexYB =
@@ -178,35 +199,45 @@ const handleProjects = ({ projects, indexY = 0, indexX = 0 }) => {
   return [
     //{
     //   caption: pictureAA.caption,
-    //  picure: `/static/${projectA.name}/${pictureAA.name}`
+    //  picture: `/static/${projectA.name}/${pictureAA.name}`
     //},
     {
       caption: pictureAB.caption,
-      picure: `/static/${projectA.name}/${pictureAB.name}`
+      picture: `/static/${projectA.name}/${
+        isMobile === true ? pictureAB.name : "desktop-" + pictureAB.name
+      }`
     },
     //{
     //  caption: pictureAC.caption,
-    //  picure: `/static/${projectA.name}/${pictureAC.name}`
+    //  picture: `/static/${projectA.name}/${pictureAC.name}`
     //},
     {
       caption: pictureBA.caption,
-      picure: `/static/${projectB.name}/${pictureBA.name}`
+      picture: `/static/${projectB.name}/${
+        isMobile === true ? pictureBA.name : "desktop-" + pictureBA.name
+      }`
     },
     {
       caption: pictureBB.caption,
-      picure: `/static/${projectB.name}/${pictureBB.name}`
+      picture: `/static/${projectB.name}/${
+        isMobile === true ? pictureBB.name : "desktop-" + pictureBB.name
+      }`
     },
     {
       caption: pictureBC.caption,
-      picure: `/static/${projectB.name}/${pictureBC.name}`
+      picture: `/static/${projectB.name}/${
+        isMobile === true ? pictureBC.name : "desktop-" + pictureBC.name
+      }`
     },
     //{
     //  caption: pictureCA.caption,
-    //  picure: `/static/${projectC.name}/${pictureCA.name}`
+    //  picture: `/static/${projectC.name}/${pictureCA.name}`
     //},
     {
       caption: pictureCB.caption,
-      picure: `/static/${projectC.name}/${pictureCB.name}`
+      picture: `/static/${projectC.name}/${
+        isMobile === true ? pictureCB.name : "desktop-" + pictureCB.name
+      }`
     }
     //{
     //  caption: pictureCC.caption,
@@ -220,9 +251,9 @@ export default class extends Component {
     super(props);
     this.state = {
       introHide: true,
-      projects: handleProjects({ projects }),
       x: 0,
       y: 0,
+      projects: [],
       transition: "all",
       swipeAction: ""
     };
@@ -233,15 +264,19 @@ export default class extends Component {
       let direction = "";
       switch (event.keyCode) {
         case 38:
+          event.preventDefault();
           direction = "Up";
           break;
         case 39:
+          event.preventDefault();
           direction = "Right";
           break;
         case 37:
+          event.preventDefault();
           direction = "Left";
           break;
         case 40:
+          event.preventDefault();
           direction = "Down";
           break;
         default:
@@ -259,6 +294,7 @@ export default class extends Component {
     };
 
     this.handleTouchEnd = event => {
+      event.preventDefault();
       const cord = {
         x: event.changedTouches[0].clientX,
         y: event.changedTouches[0].clientY
@@ -307,6 +343,7 @@ export default class extends Component {
     };
 
     this.handleScroll = event => {
+      event.preventDefault();
       if (event.deltaY > 0) {
         this.setState(state => ({ ...state, swipeAction: `slideUp` }));
         this.handleSwiperTransition();
@@ -354,7 +391,8 @@ export default class extends Component {
         projects: handleProjects({
           projects,
           indexX: state.x,
-          indexY: state.y
+          indexY: state.y,
+          isMobile
         })
       }));
       await delay(100); // ser gut
@@ -372,6 +410,11 @@ export default class extends Component {
       });
       localStorage.setItem("introHasPlayed", true);
     }
+    this.setState(state => ({
+      ...state,
+      projects: handleProjects({ projects, isMobile }),
+      isMobile
+    }));
 
     if (isMobile) {
       document.addEventListener("touchstart", this.handleTouchStart);
@@ -385,7 +428,7 @@ export default class extends Component {
   }
 
   componentWillUnmount() {
-    if (isMobile) {
+    if (this.state.isMobile) {
       document.removeEventListener("touchstart", this.handleTouchStart);
       document.removeEventListener("touchend", this.handleTouchEnd);
     } else {
@@ -399,20 +442,23 @@ export default class extends Component {
   render() {
     return (
       <div>
-        {swiper({ ...this.state })}
+        {this.state.projects.length !== 0 && swiper({ ...this.state })}
 
-        {!this.state.introHide && (
+        {this.state.introHide && (
           <section className="introSection">
             <section
               className="introOverlay"
               onTouchStart={this.hideIntro}
               onClick={this.hideIntro}
             />
-            <Intro />
+            {intro({ ...this.state })}
           </section>
         )}
         <style global jsx>{`
           body {
+            font-family: "HelveticaNeue-Light", "Helvetica Neue Light",
+              "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+            background: #282a36;
             overflow: hidden;
             width: 100vw;
             height: 100vh;
@@ -451,8 +497,8 @@ export default class extends Component {
           }
 
           ul {
-            height: ${isMobile === false ? "240vh" : "300vh"};
-            width: ${isMobile === false ? "240vw" : "300vw"};
+            height: ${this.state.isMobile === false ? "240vh" : "300vh"};
+            width: ${this.state.isMobile === false ? "240vw" : "300vw"};
             list-style: none;
             margin: 0;
             transition: ${this.state.transition} 0.2s ease-out;
@@ -460,39 +506,55 @@ export default class extends Component {
             display: grid;
             z-index: 1;
             position: fixed;
-            top: ${isMobile === false ? "-70vh" : "-100vh"};
-            left: ${isMobile === false ? "-70vw" : "-100vw"};
-            grid-template-columns: ${isMobile === false
+            top: ${this.state.isMobile === false ? "-70vh" : "-100vh"};
+            left: ${this.state.isMobile === false ? "-70vw" : "-100vw"};
+            grid-template-columns: ${this.state.isMobile === false
               ? "80vw 80vw 80vw"
               : "100vw 100vw 100vw"};
-            grid-template-rows: ${isMobile === false
+            grid-template-rows: ${this.state.isMobile === false
               ? "80vh 80vh 80vh"
               : "100vh 100vh 100vh"};
           }
 
           ul.slideLeft {
-            left: ${isMobile === false ? "10vw" : "0"};
+            left: ${this.state.isMobile === false ? "10vw" : "0"};
           }
 
           ul.slideUp {
-            top: ${isMobile === false ? "10vh" : "0"};
+            top: ${this.state.isMobile === false ? "10vh" : "0"};
           }
 
           ul.slideRight {
-            left: ${isMobile === false ? "-150vw" : "-200vw"};
+            left: ${this.state.isMobile === false ? "-150vw" : "-200vw"};
           }
 
           ul.slideDown {
-            top: ${isMobile === false ? "-150vh" : "-200vh"};
+            top: ${this.state.isMobile === false ? "-150vh" : "-200vh"};
           }
 
           li {
-            margin: ${isMobile === false ? "2.5vh 2.5vw" : "0"};
-            height: ${isMobile === false ? "75vh" : "100vh"};
-            width: ${isMobile === false ? "75vw" : "100vw"};
-            background: blue;
+            margin: ${this.state.isMobile === false ? "2.5vh 2.5vw" : "0"};
+            height: ${this.state.isMobile === false ? "75vh" : "100vh"};
+            width: ${this.state.isMobile === false ? "75vw" : "100vw"};
+            background: #f8f8f2;
             padding: 0;
             display: block;
+            border-radius: ${this.state.isMobile === false ? "10px" : "0"};
+            overflow: hidden;
+          }
+
+          img {
+            outline: none;
+            border: none;
+            object-fit: cover;
+          }
+
+          figcaption {
+            display: flex;
+            flex-direction: column;
+            align-item: center;
+            text-align: center;
+            justify-content: center;
           }
 
           li:nth-child(1) {
